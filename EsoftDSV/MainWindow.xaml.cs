8c5617c1ec09e0221cf8d844b5372b4e49478f62
Darovskikh Saveliy <atom.steel@mail.ru>
2024-01-23 23:06:33 +0300

    Добавьте файлы проекта.

---
+++ EsoftDSV/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using EsoftDSV.View;
+
+namespace EsoftDSV
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void MainWindowControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnCloseMain_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnMinimizeMain_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow loginWindow = new LoginWindow();
+            loginWindow.Show();
+            this.Close();
+        }
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new View.generalPage());
+        }
+
+        private void btnTasks_Click(object sender, RoutedEventArgs e)
+        {
+            mainFrame.Navigate(new View.taskPage());
+        }
+    }
+}
